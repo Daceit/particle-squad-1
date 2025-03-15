96beaa68f714879c80fc6e2c79e9e4e4e26e4c3e
kuang_1017333 <1017333@my.scccd.edu>
2025-03-14 20:21:53 -0700

    Added Skeleton Methods

--- particleGraphics.h
+++ particleGraphics.h
@@ -1,2 +1,34 @@
 #include <iostream>
 using namespace std;
+
+class PartGraph {
+	string color;
+
+	public:
+		PartGraph(string new_color) {
+			color = new_color;
+		}
+		void set_color(const string& new_color) {
+			color = new_color;
+		}
+		string get_color() const {
+			return color;
+		}
+		void draw_point() {
+			cout << "Draw point\n";
+		}
+		void draw_rectangle() {
+			cout << "Draw rectangle\n";
+		}
+		void draw_oval() {
+			cout << "Draw oval\n";
+		}
+		void draw_polygon() {
+			cout << "Draw polygon\n";
+		}
+		void draw_line() {
+			cout << "Draw line\n";
+		}
+
+
+};
