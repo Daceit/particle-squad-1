3830e4a385e44f9223eb181193c4e1b121ed1b7e
ngo_1019461 <1019461@my.scccd.edu>
2025-03-16 13:47:13 -0700

    message

--- particleSystem.h
+++ particleSystem.h
@@ -1,1 +1,18 @@
 #include <iostream>
+using namespace std;
+class particleSystem {
+	int columns, rows; 
+	Particle* head;
+	Particle* tail; 
+	int count; 
+
+public: 
+	ParticleSystem(int cols, int rows) : columns(cols), rows(rows), head(nullptr), tail(nullptr), count(0){}
+	~ParticleSystem
+	void addParticle(int x, int y);
+	int numParticle()const;
+	void moveParticles();
+	void drawParticles();
+	void drawWindow();
+};
+	
