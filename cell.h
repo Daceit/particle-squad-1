ba8fe49accde1c47b21d941388c8d371ee850afb
Talbot_1033389 <1033389@my.scccd.edu>
2025-03-17 01:13:39 -0700

    finished Cell class (double linked list)

--- cell.h
+++ cell.h
@@ -1,1 +1,50 @@
-// hi
+#include <iostream>
+#include "Particle.h"
+using namespace std;
+
+class Cell {
+private:
+	// particle being stored in each cell
+	Particle data;
+	
+	// initialize prev and next pointers to nullptr
+	Cell* next = nullptr;
+	Cell* prev = nullptr;
+
+public:
+	// constructor, copies particle by reference to "particle var", 
+	// sets data to particle object, and sets prev and next to nullptr
+	Cell(const Particle& particle) : data(particle), prev(nullptr), next(nullptr){
+	}
+
+	// getter for data, const bc wont change value
+	Particle getData() const{
+		return data;
+	}
+
+	// getter for prev, const bc wont change value
+	Cell* getPrev() const{
+		return prev;
+	}
+
+	// getter for next, const bc wont change value
+	Cell* getNext() const{
+		return next;
+	}
+
+	// setter for data, passing Particle object 
+	// const and by reference, no extra copys and not changing original
+	void setData(const Particle& particle){
+		data = particle;
+	}
+
+	// setter for prev, (Cell pointer)
+	void setPrev(Cell* cell){
+		prev = cell;
+	}
+
+	// setter for next, (cell pointer)
+	void setNext(Cell* cell){
+		next = cell;
+	}
+};
