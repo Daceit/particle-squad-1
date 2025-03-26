bfd42a8ecf37f28cee5bc5dd25a9822576520beb
1014109 <matthewsullivan05@gmail.com>
2025-03-25 18:45:03 -0700

    COMMENT

--- particle.h
+++ particle.h
@@ -12,47 +12,63 @@ class Particle {
 	int lifetime = 50;
 	string movement_type = "UNSET"; // STREAMER  BALLISTIC  FIREWORK
 	public:
+	//constructs particle
 	Particle(){}
+	// allows constructor to tack in diffrent ammounts of arguments
 	Particle(int Nx, int Ny) : x{Nx}, y{Ny}{}
 	Particle (int Nx, int Ny, int NvelX,int NvelY, int Nlif, string Nmov) : x(Nx), y(Ny) , velocityX(NvelX), velocityY(NvelY), lifetime(Nlif), movement_type(Nmov){}
+	// gets y axis
 	int get_y(){
 		return y;
 	}
+	// gets x axis
 	int get_x(){
 		return x;
 	}
+	// returns X velovity
 	int get_velocityX(){
 		return velocityX;
 	}
+	// returns Y velocity
 	int get_velocityY(){
 		return velocityY;
 	}
+	// returns the lifetime
 	int get_lifetime(){
 		return lifetime;
 	}
+	// reurns the movement type
 	string get_movement_type(){
 		return movement_type;
 	}
+	// set x axis
 	void set_x(int Nx){
 		x = Nx;
 	}
+	// sets y axis
 	void set_y(int Ny){
 		y = Ny;
 	}
+	// sets the X velocity
 	void set_velocityX(int Npos){
 		velocityX = Npos;
 	}
+	// sets the Y velocity
 	void set_velocityY(int Npos){
 		velocityY = Npos;
 	}
+
+	// sets liftime
 	void set_lifetime(int Npos){
 		lifetime = Npos;
 	}
+	// sets movement type
 	void set_movement_type(string Npos){
 		movement_type = Npos;
 	}
 	
 	void physics(){
+		// checks sets the type of movement fot the particle
 		if(movement_type == "BALLISTIC"){
 			usleep(1000000);
 			lifetime--;
@@ -67,6 +83,7 @@ class Particle {
 			lifetime--;
 			y += velocityY;
 		} else {
+			//displays when movement type is not set
 			cout << "set movement_type to STREAMER_X, STREAMER_Y, BALLISTIC, FIREWORK_X or FIREWORK_Y" << endl;
 		}
 	}
