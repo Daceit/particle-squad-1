dae1df5a6017125a34994775249e77d112c91b4a
Raymondkngo <raykhanhh@gmail.com>
2025-03-26 12:55:52 -0700

    Update rain.h

--- rain.h
+++ rain.h
@@ -1,17 +1,28 @@
-#pragma once
+##pragma once
 #include "particleSystem.h"
 #include "particleGraphics.h"
 #include <cstdlib> //rand()
 #include <ctime> //time(nullptr) random number gen
 #include <thread> //control fps
 #include <chrono> //used with thread
+#include <unistd.h> //usleep
 
-
-class Rain{ 
-	particleSystem ps;
-	int x1, y1, x2, y2; 
-
-public: 
-	Rain(int x_start = 5, int y_start = 3, int x_end = 70, int y_end = 25) : x1(x_start), y1(y_start), x2(x_end), y2(y_end) {
-	std::srand(std::time(nullptr)
-	}
+void rain() {
+    clearscreen();
+    auto [rows, cols] = get_terminal_size();
+    particleSystem ps(rows - 1, cols - 1);
+    std::srand(std::time(0));
+    PartGraphic pg;
+    for (int frame = 0; frame < 500; frame++) {
+        for (int i = 0; i < 10; i++) {
+            int x = std::rand() % (cols - 1);
+            int y = 0;
+            int z = 0, v = 1, e = rows;
+            std::string movement = "STREAMER_Y";
+            ps.addParticle(x, y, z, v, e, movement);
+            pg.draw_rectangle(0, 0, cols - 1, rows - 1, true);
+        }
+        ps.moveParticles();
+        usleep(1'000'000/5);
+    }
+};
